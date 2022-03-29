8a343e6bef43529929307163e34939bd39382b06
mu0363 <dde036@gmail.com>
2022-03-29 13:23:37 +0900

    :sparkles: feat: setup graphql module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLDateTime } from 'graphql-iso-date';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // ディフォルトのplaygroundではなくApolloSandboxを使用する
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      // Schema firstのアプローチのため、スキーマ定義ファイルの場所を定義する
+      typePaths: ['./**/*.graphql'],
+      // graphql-iso-dateのDateTimeを使用する
+      // graphqlスキーマファイルのDateTimeと関連する
+      resolvers: { DateTime: GraphQLDateTime },
+    }),
+    DonationsModule,
+  ],
   controllers: [],
   providers: [],
 })
