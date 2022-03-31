723748a453ee7856834366e0798143e69496998b
mu0363 <dde036@gmail.com>
2022-03-31 16:01:32 +0900

    :bulb: fix: initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,12 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { GraphQLDateTime } from 'graphql-iso-date';
-import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // ディフォルトのplaygroundではなくApolloSandboxを使用する
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      // Schema firstのアプローチのため、スキーマ定義ファイルの場所を定義する
-      typePaths: ['./**/*.graphql'],
-      // graphql-iso-dateのDateTimeを使用する
-      // graphqlスキーマファイルのDateTimeと関連する
-      resolvers: { DateTime: GraphQLDateTime },
     }),
-    DonationsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
--- src/donations/donations.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DonationsService } from './donations.service';
-import { DonationsResolver } from './donations.resolver';
-
-@Module({
-  providers: [DonationsResolver, DonationsService]
-})
-export class DonationsModule {}
